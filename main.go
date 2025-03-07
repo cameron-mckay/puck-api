b738c9c03681723f429f53d4d7c4248d9c3eecab
Cameron <cameronmckay62@gmail.com>
2025-03-07 16:47:36 -0700

    puck it

--- main.go
+++ main.go
@@ -1,11 +1,11 @@
 package main
 
 import (
+	"encoding/json"
 	"log"
 	"net/http"
 	"os"
 
-	"github.com/gorilla/mux"
 	"github.com/joho/godotenv"
 	"otherworldly.dev/puck-api/monnitapi"
 )
@@ -15,12 +15,45 @@ func main() {
 	if err != nil {
 		log.Fatalf("Error loading .env file: %v", err)
 	}
-	monnitapi.InitApiHandler(os.Getenv("API_KEY_ID"), os.Getenv("API_KEY_SECRET"), 10)
-	r := mux.NewRouter()
-	r.HandleFunc("/", HomeHandler)
-	http.ListenAndServe(":42069", r)
+	monnitapi.InitApiHandler(os.Getenv("BASE_URL"), os.Getenv("API_KEY_ID"), os.Getenv("API_KEY_SECRET"))
+	http.HandleFunc("/", HomeHandler)
+	http.HandleFunc("/deleteOnNetwork", DeleteAllSensorsOnNetwork)
+	http.ListenAndServe(":42069", nil)
 }
 
 func HomeHandler(w http.ResponseWriter, r *http.Request) {
 	http.ServeFile(w, r, "static/index.html")
 }
+
+
+type DeleteSensorsOnNetwork struct {
+	NetworkID int `json:"networkId"`
+}
+func DeleteAllSensorsOnNetwork(w http.ResponseWriter, r *http.Request) { 
+	w.Header().Set("Content-Type", "application/json")
+
+	defer r.Body.Close()
+
+	decodedReq := new(DeleteSensorsOnNetwork)
+	
+	err := json.NewDecoder(r.Body).Decode(decodedReq)
+
+	// Check for errors
+	if err != nil {
+		log.Fatalf("Error decoding request: %v", err)
+		w.Write([]byte("Error decoding request"))
+	}
+	// Create request body
+	sensors := monnitapi.GetSensorsOnNetwork(decodedReq.NetworkID)
+
+	for i := range len(sensors.Result) {
+		id := sensors.Result[i].SensorID
+		res := monnitapi.RemoveSensor(id)
+		if res == false {
+			log.Fatalf("Error on sensor ID %d", id)
+		}
+	}
+
+	w.Write([]byte("Success"))
+}
+
--- monnitapi/monnitapi.go
+++ monnitapi/monnitapi.go
@@ -1,50 +1,235 @@
 package monnitapi
 
 import (
+	"bytes"
+	"encoding/json"
 	"log"
 	"net/http"
 	"time"
 )
 
-var requestChannel chan *http.Request
-var responseChannel chan *http.Response
+var requestQueue chan *http.Request
+var resultChannel chan *http.Response
 var httpClient *http.Client
+var baseUrl string
 
-func InitApiHandler(apiKeyId string, apiKeySecret string, timeOutSeconds int) {
+// Initializes variables required for API communcation
+func InitApiHandler(url string, apiKeyId string, apiKeySecret string) {
+	baseUrl = url
 	// Create non default http client
-	httpClient = &http.Client{Timeout: time.Duration(timeOutSeconds) * time.Second}
+	httpClient = &http.Client{Timeout: 10 * time.Second}
 
 	// Create API queue channel
-	requestChannel = make(chan *http.Request, 1)
-	responseChannel = make(chan *http.Response, 1)
+	requestQueue = make(chan *http.Request, 1)
+	resultChannel = make(chan *http.Response, 1)
 
-	// Create request handler goroutine
+	//
 	go func() {
-		for query := range requestChannel {
+		// Watch the request queue
+		for query := range requestQueue {
+			// Add api auth
 			query.Header.Add("APIKeyID", apiKeyId)
 			query.Header.Add("APISecretKey", apiKeySecret)
+
+			// Set content type
+			query.Header.Set("Content-Type", "application/json")
+
+			// Run the request
 			response, err := httpClient.Do(query)
+
+			// Return result to channel
 			if err != nil {
 				log.Fatalln("Error processing request: ", err)
-				responseChannel <- nil
+				resultChannel <- nil
 			} else {
-				responseChannel <- response
+				resultChannel <- response
 			}
 		}
 	}()
 }
 
+// Closes request handler channels
 func CloseApiHandler() {
-	// Close request queues
-	close(requestChannel)
-	close(responseChannel)
+	close(requestQueue)
+	close(resultChannel)
 }
 
-func GatewayReform(gatwayId int) bool {
+type genericApiResponse struct {
+	Method string `json:"Method"`
+	Result string `json:"Result"`
+}
 
-	// resp, err := http.Get("")
-	// if err != nil {
+type gatewayReformReq struct {
+	GatewayID int `json:"GatewayID"`
+}
 
-	// }
-	return true
+func GatewayReform(gatewayId int) bool {
+	// Create request body
+	body := &gatewayReformReq{
+		GatewayID: gatewayId,
+	}
+
+	// Create the payload buffer and copy to it
+	payloadBuffer := new(bytes.Buffer)
+	json.NewEncoder(payloadBuffer).Encode(body)
+
+	// Create the request
+	req, err := http.NewRequest(http.MethodPost, baseUrl+"/GatewayReform", payloadBuffer)
+
+	// Check for errors
+	if err != nil {
+		log.Fatalf("Could not create gateway reform post request: %v", err)
+		return false
+	}
+	requestQueue <- req
+	response := <-resultChannel
+
+	if response == nil {
+		log.Fatalln("Gateway reform response is null")
+		return false
+	}
+
+	// Close the body when done
+	defer response.Body.Close()
+
+	// Create the response object
+	decodedRes := new(genericApiResponse)
+
+	// Decode buffer to object
+	err = json.NewDecoder(response.Body).Decode(decodedRes)
+
+	// Check for errors
+	if err != nil {
+		log.Fatalf("Error decoding gateway reform response: %v", err)
+		return false
+	}
+
+	return decodedRes.Result == "Success"
+}
+
+type sensorListReq struct {
+	NetworkID int `json:"NetworkID"`
+	AccountID int `json:"accountID"`
+}
+
+type SensorListResponse struct {
+	Method string `json:"Method"`
+	Result []ApiSensor `json:"Result"`
+}
+
+type ApiSensor struct {
+	SensorID             int    `json:"SensorID"`
+	ApplicationID        int    `json:"ApplicationID"`
+	CSNetID              int    `json:"CSNetID"`
+	SensorName           string `json:"SensorName"`
+	LastCommuncationDate string `json:"LastCommunicationDate"`
+	NextCommuncationDate string `json:"NextCommunicationDate"`
+	LastDataMessageID    int    `json:"LastDataMessageID"`
+	PowerSourceID        int    `json:"PowerSourceID"`
+	Status               int    `json:"Status"`
+	CanUpdate            bool   `json:"CanUpdate"`
+	CurrentReading       string `json:"CurrentReading"`
+	BatteryLevel         int    `json:"BatteryLevel"`
+	SignalStrength       int    `json:"SignalStrength"`
+	AlertsActive         bool   `json:"AlertsActive"`
+	CheckDigit           string `json:"CheckDigit"`
+	AccountID            int    `json:"AccountID"`
+	MonnitApplicationID  int    `json:"MonnitApplicationID"`
+}
+
+func GetSensorsOnNetwork(networkId int) *SensorListResponse {
+	// Create request body
+	body := &sensorListReq{
+		NetworkID: networkId,
+		AccountID: 73221,
+	}
+
+	// Create the payload buffer and copy to it
+	payloadBuffer := new(bytes.Buffer)
+	json.NewEncoder(payloadBuffer).Encode(body)
+
+	// Create the request
+	req, err := http.NewRequest(http.MethodPost, baseUrl+"/SensorList", payloadBuffer)
+
+	// Check for errors
+	if err != nil {
+		log.Fatalf("Could not create gateway reform post request: %v", err)
+		return nil
+	}
+	requestQueue <- req
+	response := <-resultChannel
+
+	if response == nil {
+		log.Fatalln("Gateway reform response is null")
+		return nil
+	}
+
+	// Close the body when done
+	defer response.Body.Close()
+
+	// Create the response object
+	decodedRes := new(SensorListResponse)
+
+	res := response.Body
+
+	// Decode buffer to object
+	err = json.NewDecoder(res).Decode(decodedRes)
+
+	// Check for errors
+	if err != nil {
+		log.Fatalf("Error decoding gateway reform response: %v", err)
+		return nil
+	}
+
+	// Check if reform is successful
+	return decodedRes
+}
+
+
+type removeSensorReq struct {
+	SensorID int `json:"SensorID"`
+}
+
+func RemoveSensor(sensorId int) bool {
+	// Create request body
+	body := &removeSensorReq{
+		SensorID: sensorId,
+	}
+
+	// Create the payload buffer and copy to it
+	payloadBuffer := new(bytes.Buffer)
+	json.NewEncoder(payloadBuffer).Encode(body)
+
+	// Create the request
+	req, err := http.NewRequest(http.MethodPost, baseUrl+"/RemoveSensor", payloadBuffer)
+
+	// Check for errors
+	if err != nil {
+		log.Fatalf("Could not create remove sensor post request: %v", err)
+		return false
+	}
+	requestQueue <- req
+	response := <-resultChannel
+
+	if response == nil {
+		log.Fatalln("Remove sensor response is null")
+		return false
+	}
+
+	// Close the body when done
+	defer response.Body.Close()
+
+	// Create the response object
+	decodedRes := new(genericApiResponse)
+
+	// Decode buffer to object
+	err = json.NewDecoder(response.Body).Decode(decodedRes)
+
+	// Check for errors
+	if err != nil {
+		log.Fatalf("Error decoding sensor remove response: %v", err)
+		return false
+	}
+
+	return decodedRes.Result == "Success"
 }
